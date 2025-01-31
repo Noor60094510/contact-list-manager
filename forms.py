702d0d68cded29cc61ea12d03e0fb291e0059bde
Nausheenkhan-git <60301585@udst.edu.qa>
2025-01-31 17:59:52 +0300

    added email validation to prevent invalid emails

--- forms.py
+++ forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
-    name = StringField('Name')
-    phone = StringField('Phone')
-    email = StringField('Email')
+    name = StringField('Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired()])
+    email = StringField('Email', validators=[Email()])
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
-                              ('Work', 'Work'), 
-                              ('Other', 'Other')])
-    submit = SubmitField('Submit') 
+                               ('Work', 'Work'), 
+                               ('Other', 'Other')],
+                      validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
